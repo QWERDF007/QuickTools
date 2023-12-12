3f818270bf4510804fbc193744faa806d3b625f0
wangtao <595517713@qq.com>
2023-12-13 00:07:40 +0800

    添加图像处理插件库

---
+++ src/imgproc/imagehistogram.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <QObject>
+
+class ImageHistogram : QObject
+{
+    ImageHistogram(QObject *parent = nullptr) {}
+
+    ~ImageHistogram() {}
+};
---
+++ src/imgproc/qml/ImageHistogram.qml
@@ -0,0 +1,7 @@
+import QtQuick
+import QuickTools.stats
+
+Rectangle {
+    width: 200
+    height: 200
+}
