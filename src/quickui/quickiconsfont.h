f478a70133d1cdcf17c0658b9fa7c03df215802e
wangtao <595517713@qq.com>
2023-12-21 00:02:52 +0800

    添加图标字体

--- src/quickui/quickiconsfont.h
+++ src/quickui/quickiconsfont.h
@@ -16,6 +16,13 @@ enum class FontIconType
     CheckboxComposite = 0xe73a, //!< 复选框, 勾选
     Home              = 0xe80f, //!< 主页
     QRCode            = 0xed14, //!< 二维码
+    ReportHacked      = 0xe730, //!< 报告黑客入侵
+    Airplane          = 0xe709, //!< 飞机
+    ShoppingCart      = 0xe7bf, //!< 购物车
+    Train             = 0xe7c0, //!< 火车
+    Flag              = 0xe7c1, //!< 标记
+    Ferry             = 0xe7e3, //!< 渡船
+    Calculator        = 0xe8ef, //!< 计算器
 };
 Q_ENUM_NS(FontIconType) // 向元对象系统注册枚举类型，必须在 Q_NAMESPACE 宏声明的命名空间中
 QML_NAMED_ELEMENT(QuickFontIcon) // 声明封闭类型或命名空间在 QML 中可用，以 name 进行访问
