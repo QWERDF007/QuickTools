ca813f3899017e40d3270e893aac750535213df6
wangtao <595517713@qq.com>
2023-12-21 00:02:21 +0800

    添加字体

--- src/quickui/quickfont.cpp
+++ src/quickui/quickfont.cpp
@@ -7,15 +7,41 @@ QuickFont *QuickFont::instance_ = nullptr;
 QuickFont::QuickFont(QObject *parent)
     : QObject(parent)
 {
+    QFont caption;
+    caption.setPixelSize(12);
+    Caption(caption);
+
     QFont body;
     body.setPixelSize(13);
     Body(body);
+
+    QFont bodyStrong;
+    bodyStrong.setPixelSize(13);
+    bodyStrong.setWeight(QFont::DemiBold);
+    BodyStrong(bodyStrong);
+
+    QFont subtitle;
+    subtitle.setPixelSize(20);
+    subtitle.setWeight(QFont::DemiBold);
+    Subtitle(subtitle);
+
+    QFont title;
+    title.setPixelSize(28);
+    title.setWeight(QFont::DemiBold);
+    Title(title);
+
+    QFont titleLarge;
+    titleLarge.setPixelSize(40);
+    titleLarge.setWeight(QFont::DemiBold);
+    TitleLarge(titleLarge);
+
+    QFont display;
+    display.setPixelSize(68);
+    display.setWeight(QFont::DemiBold);
+    Display(display);
 }
 
-QuickFont::~QuickFont()
-{
-
-}
+QuickFont::~QuickFont() {}
 
 QuickFont *QuickFont::getInstance()
 {
@@ -25,7 +51,5 @@ QuickFont *QuickFont::getInstance()
     }
     return instance_;
 }
-
-
 
 } // namespace QuickTools::ui
--- src/quickui/quickfont.h
+++ src/quickui/quickfont.h
@@ -12,7 +12,13 @@ class QuickFont : public QObject
 {
     Q_OBJECT
 
-    Q_PROPERTY_AUTO(QFont, Body)
+    Q_PROPERTY_AUTO(QFont, Caption);
+    Q_PROPERTY_AUTO(QFont, Body);
+    Q_PROPERTY_AUTO(QFont, BodyStrong);
+    Q_PROPERTY_AUTO(QFont, Subtitle);
+    Q_PROPERTY_AUTO(QFont, Title);
+    Q_PROPERTY_AUTO(QFont, TitleLarge);
+    Q_PROPERTY_AUTO(QFont, Display);
 
     /**
      * 在 QML 中通过 QuickColor 可访问本单例
