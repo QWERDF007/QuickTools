f7599a3c7922c6ef0dfd8d3b41fd429351bdc026
wangtao <595517713@qq.com>
2023-12-25 00:23:13 +0800

    添加工具基类接口，参数基类接口

--- src/toolstypes/abstractquicktool.cpp
+++ src/toolstypes/abstractquicktool.cpp
@@ -1,1 +1,34 @@
 #include "abstractquicktool.h"
+
+namespace quicktools {
+
+QHash<int, QByteArray> quicktools::AbstractToolParams::roleNames() const
+{
+    return {
+        {ParamRole::ParamNameRole, "name"},
+        {ParamRole::ParamTypeRole, "type"},
+        {ParamRole::ParamValueRole, "value"},
+        };
+}
+
+QVariant AbstractToolParams::data(const QModelIndex &index, int role) const
+{
+    if (index.row() < 0 || index.row() >= params_.size())
+        return QVariant();
+    switch (role) {
+    case ParamRole::ParamNameRole:
+
+        break;
+    default:
+        break;
+    }
+
+    return QVariant();
+}
+
+bool AbstractToolParams::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    return false;
+}
+
+} // namespace quicktools
--- src/toolstypes/abstractquicktool.h
+++ src/toolstypes/abstractquicktool.h
@@ -1,15 +1,61 @@
 #pragma once
 
 #include <QObject>
+#include <QAbstractListModel>
 
 namespace quicktools {
 
 class AbstractQuickTool : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QAbstractListModel* params READ params CONSTANT)
 public:
-    AbstractQuickTool(QObject *parent = nullptr) : QObject(parent) {}
+    AbstractQuickTool(QObject *parent = nullptr)
+        : QObject(parent)
+    {
+    }
+
     virtual ~AbstractQuickTool() = default;
+
+    virtual QAbstractListModel* params() = 0;
+
+    int operator()()
+    {
+        return run();
+    }
+
+    virtual int run() = 0;
+
+private:
+    Q_DISABLE_COPY(AbstractQuickTool)
+};
+
+class AbstractToolParams : QAbstractListModel
+{
+public:
+    AbstractToolParams(QObject *parent = nullptr) : QAbstractListModel(parent) {}
+    ~AbstractToolParams() = default;
+
+    enum ParamRole
+    {
+        ParamNameRole = Qt::UserRole + 1,
+        ParamTypeRole,
+        ParamValueRole,
+    };
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override
+    {
+        if (parent.isValid())
+            return 0;
+        return params_.size();
+    }
+
+    QHash<int, QByteArray> roleNames() const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
+
+protected:
+    QVector<QMap<int, QVariant>> params_;
 };
 
 } // namespace quicktools
