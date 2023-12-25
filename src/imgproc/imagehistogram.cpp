b6b395fbff3f1761337195586bad2e633574472e
wangtao <595517713@qq.com>
2023-12-26 01:16:09 +0800

    继承抽象工具类

--- src/imgproc/imagehistogram.cpp
+++ src/imgproc/imagehistogram.cpp
@@ -4,38 +4,21 @@ namespace quicktools {
 
 namespace {
 
-enum ParamRole {
-    NameRole =  Qt::UserRole + 1,
-};
-}
-
-ImageHistogram::ImageHistogram(QObject *parent) : AbstractQuickTool(parent) {}
-
-QAbstractListModel *ImageHistogram::params()
+enum ParamRole
 {
-    return params_;
+    NameRole = Qt::UserRole + 1,
+};
+} // namespace
+
+ImageHistogram::ImageHistogram(QObject *parent)
+    : AbstractQuickTool(parent)
+{
 }
+
 
 int ImageHistogram::run()
 {
     return 0;
-}
-
-int ImageHistogram::ImageHistogramParams::rowCount(const QModelIndex &parent) const
-{
-    return 0;
-}
-
-QHash<int, QByteArray> ImageHistogram::ImageHistogramParams::roleNames() const
-{
-    return {
-        {NameRole, "name"},
-    };
-}
-
-QVariant ImageHistogram::ImageHistogramParams::data(const QModelIndex &index, int role) const
-{
-    return QVariant();
 }
 
 } // namespace quicktools
--- src/imgproc/imagehistogram.h
+++ src/imgproc/imagehistogram.h
@@ -5,32 +5,20 @@
 #include <QObject>
 
 namespace quicktools {
-class ImageHistogram : AbstractQuickTool
+
+class ImageHistogram : public AbstractQuickTool
 {
+    Q_OBJECT
 public:
     ImageHistogram(QObject *parent = nullptr);
 
     ~ImageHistogram() = default;
 
-    QAbstractListModel * params() override;
 
     int run() override;
 
 private:
-    class ImageHistogramParams : public QAbstractListModel
-    {
-    public:
-        ImageHistogramParams(QObject *parent = nullptr) : QAbstractListModel(parent) {}
-        ~ImageHistogramParams() = default;
 
-        int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-        QHash<int, QByteArray> roleNames() const override;
-        QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-
-    };
-
-    ImageHistogramParams* params_{nullptr};
 };
 
 } // namespace quicktools
-
