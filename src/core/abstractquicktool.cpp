58c0e8700e98c12246995ca64cb8e64c1cb72a9b
wangtao <595517713@qq.com>
2024-01-07 23:42:58 +0800

    添加参数类型，添加参数相关接口，添加输入图像源

--- src/core/abstractquicktool.cpp
+++ src/core/abstractquicktool.cpp
@@ -1,28 +1,59 @@
 #include "abstractquicktool.h"
 
+#include <unordered_map>
+
 namespace quicktools {
 
 QuickToolFactor *QuickToolFactor::instance_ = nullptr;
+
+AbstractToolParams::~AbstractToolParams()
+{
+    qInfo() << __FUNCTION__ << this;
+}
+
+int AbstractToolParams::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid())
+        return 0;
+    return params_.size();
+}
 
 QVariant AbstractToolParams::data(const QModelIndex &index, int role) const
 {
     if (index.row() < 0 || index.row() >= params_.size())
         return QVariant();
-    switch (role)
-    {
-    case ParamNameRole:
-
-        break;
-    default:
-        break;
-    }
-
-    return QVariant();
+    return params_.at(index.row()).value(role, QVariant());
 }
 
 bool AbstractToolParams::setData(const QModelIndex &index, const QVariant &value, int role)
 {
+    if (index.row() < 0 || index.row() >= params_.size())
+        return false;
+    switch (role)
+    {
+    case ParamVisibleRole:
+    case ParamValueRole:
+        params_[index.row()][role] = value;
+        return true;
+        break;
+    default:
+        break;
+    }
     return false;
+}
+
+bool AbstractToolParams::addParam(const QString &name, const int type, const QVariant &value, const QVariant &range,
+                                  const QVariant &visible)
+{
+    QMap<int, QVariant> param{
+        {   ParamNameRole,    name},
+        {   ParamTypeRole,    type},
+        {ParamVisibleRole, visible},
+        {  ParamValueRole,   value},
+        {  ParamRangeRole,   range},
+    };
+    params_.emplace_back(param);
+    return true;
 }
 
 AbstractQuickTool::AbstractQuickTool(QObject *parent)
@@ -57,13 +88,33 @@ bool AbstractQuickTool::setOutputParams(AbstractToolOutputParams *output_params)
     return false;
 }
 
-QuickToolFactor *QuickToolFactor::getInstance()
+bool AbstractQuickTool::setImageSource(const QUrl &url)
 {
-    if (instance_ == nullptr)
+    if (url == image_source_)
     {
-        instance_ = new QuickToolFactor;
+        return false;
     }
-    return instance_;
+    image_source_ = url;
+    emit imageSourceChanged();
+    return true;
+}
+
+AbstractQuickTool *QuickToolFactor::createQuickTool(int type) const
+{
+    auto found = tool_creators_.find(type);
+    if (found != tool_creators_.end())
+    {
+        auto callable = found->second;
+        return callable();
+    }
+    return nullptr;
+}
+
+void QuickToolFactor::registerQuickTool(int type, AbstractQuickToolCreator creator)
+{
+    auto found = tool_creators_.find(type);
+    assert(found == tool_creators_.end() && "This type is already registerd");
+    tool_creators_.emplace(type, creator);
 }
 
 } // namespace quicktools
--- src/core/abstractquicktool.h
+++ src/core/abstractquicktool.h
@@ -17,6 +17,16 @@ enum QuickToolType
     ImageHistogram = 1, //!< 图像直方图
 };
 Q_ENUM_NS(QuickToolType) // 向元对象系统注册枚举类型，必须在 Q_NAMESPACE 宏声明的命名空间中
+
+enum QuickToolParamType
+{
+    Text = 1,      //!< 文本参数
+    IntSpinBox,    //!< 整型旋钮
+    DoubleSpinBox, //!< 浮点旋钮
+    ComboBox,      //!< 下拉列表
+};
+Q_ENUM_NS(QuickToolParamType) // 向元对象系统注册枚举类型，必须在 Q_NAMESPACE 宏声明的命名空间中
+
 QML_NAMED_ELEMENT(QuickToolType) // 声明封闭类型或命名空间在 QML 中可用，以 name 进行访问
 } // namespace quicktooltypes
 
@@ -29,7 +39,7 @@ public:
     {
     }
 
-    virtual ~AbstractToolParams() {}
+    virtual ~AbstractToolParams();
 
     enum ToolParamRole
     {
@@ -40,12 +50,7 @@ public:
         ParamRangeRole,
     };
 
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override
-    {
-        if (parent.isValid())
-            return 0;
-        return params_.size();
-    }
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
 
     QHash<int, QByteArray> roleNames() const override
     {
@@ -62,8 +67,13 @@ public:
 
     bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
 
+    bool addParam(const QString &name, const int type, const QVariant &value,
+                  const QVariant &range = QVariant::fromValue(nullptr), const QVariant &visible = true);
+
 protected:
     QVector<QMap<int, QVariant>> params_;
+    //    QSet<QString> names_;
+    //    QMap<QString, QMap<int, QVariant>> params_;
 };
 
 class QUICKTOOLS_CORE_EXPORT AbstractToolInputParams : public AbstractToolParams
@@ -102,6 +112,7 @@ class QUICKTOOLS_CORE_EXPORT AbstractQuickTool : public QObject
     Q_PROPERTY(QAbstractListModel *inputParams READ inputParams NOTIFY inputParamsChanged FINAL)
     Q_PROPERTY(QAbstractListModel *outputParams READ outputParams NOTIFY outputParamsChanged FINAL)
     Q_PROPERTY(QString name READ name NOTIFY nameChanged FINAL) // FINAL 表明该属性不会被派生类覆盖
+    Q_PROPERTY(QUrl imageSource READ imageSource WRITE setImageSource NOTIFY imageSourceChanged FINAL)
 public:
     AbstractQuickTool(QObject *parent = nullptr);
     virtual ~AbstractQuickTool();
@@ -113,7 +124,7 @@ public:
         return run();
     }
 
-    virtual int run() = 0;
+    Q_INVOKABLE virtual int run() = 0;
 
     QAbstractListModel *inputParams() const
     {
@@ -129,15 +140,27 @@ public:
 
     bool setOutputParams(AbstractToolOutputParams *output_params);
 
-private:
-    Q_DISABLE_COPY(AbstractQuickTool)
+    const QUrl &imageSource() const
+    {
+        return image_source_;
+    }
+
+    bool setImageSource(const QUrl &url);
+
+protected:
     AbstractToolInputParams  *input_params_{nullptr};
     AbstractToolOutputParams *output_params_{nullptr};
+
+    QUrl image_source_;
+
+private:
+    Q_DISABLE_COPY(AbstractQuickTool)
 
 signals:
     void inputParamsChanged();
     void outputParamsChanged();
     void nameChanged();
+    void imageSourceChanged();
 };
 
 class QUICKTOOLS_CORE_EXPORT QuickToolFactor : public QObject
@@ -155,13 +178,20 @@ class QUICKTOOLS_CORE_EXPORT QuickToolFactor : public QObject
     QML_SINGLETON
 
 public:
-    using ClassCreator = std::function<AbstractQuickTool *(void)>;
+    using AbstractQuickToolCreator = std::function<AbstractQuickTool *(void)>;
 
     /**
      * @brief 获取单例实例的指针
      * @return
      */
-    static QuickToolFactor *getInstance();
+    static QuickToolFactor *getInstance()
+    {
+        if (instance_ == nullptr)
+        {
+            instance_ = new QuickToolFactor;
+        }
+        return instance_;
+    }
 
     /**
      * @brief 提供给 QML 创建一个单例实例的静态工厂函数
@@ -176,23 +206,9 @@ public:
         return getInstance();
     }
 
-    Q_INVOKABLE AbstractQuickTool *createQuickTool(int type) const
-    {
-        auto found = tool_creators_.find(type);
-        if (found != tool_creators_.end())
-        {
-            auto callable = found->second;
-            return callable();
-        }
-        return nullptr;
-    }
+    Q_INVOKABLE AbstractQuickTool *createQuickTool(int type) const;
 
-    void registerQuickTool(int type, ClassCreator creator)
-    {
-        auto found = tool_creators_.find(type);
-        assert(found == tool_creators_.end() && "This type is already registerd");
-        tool_creators_.emplace(type, creator);
-    }
+    void registerQuickTool(int type, AbstractQuickToolCreator creator);
 
 private:
     explicit QuickToolFactor(QObject *parent = nullptr)
@@ -205,7 +221,7 @@ private:
      */
     static QuickToolFactor *instance_;
 
-    std::map<int, ClassCreator> tool_creators_;
+    std::map<int, AbstractQuickToolCreator> tool_creators_;
 
     /**
      * 禁用拷贝和移动构造和运算符
