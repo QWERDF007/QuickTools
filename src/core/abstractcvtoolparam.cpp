8161848d1b7cdb11694a8a4ada8d4550ef0e893f
wangtao <595517713@qq.com>
2024-02-01 00:27:42 +0800

    删除属性

--- src/core/abstractcvtoolparam.cpp
+++ src/core/abstractcvtoolparam.cpp
@@ -2,34 +2,4 @@
 
 namespace quicktools::core {
 
-bool AbstractCVInputParams::setInputImages(const QVariant &value)
-{
-    if (image_path_ == value)
-    {
-        return false;
-    }
-    QString path = value.toString();
-    if (path.startsWith("file:///"))
-    {
-        image_path_ = path.right(path.size() - 8);
-    }
-    else
-    {
-        image_path_ = path;
-    }
-    emit inputImagesChanged();
-    return true;
-}
-
-bool AbstractCVOutputParams::setOutputImages(const QVariant &value)
-{
-    if (output_data_ == value)
-    {
-        return false;
-    }
-    output_data_ = value;
-    emit outputImagesChanged();
-    return true;
-}
-
 } // namespace quicktools::core
--- src/core/abstractcvtoolparam.h
+++ src/core/abstractcvtoolparam.h
@@ -9,7 +9,6 @@ class QUICKTOOLS_CORE_EXPORT AbstractCVInputParams : public AbstractInputParams
     QML_NAMED_ELEMENT(CVInputParams)
     QML_UNCREATABLE("Can't not create a AbstractCVInputParams directly")
 
-    Q_PROPERTY(QVariant inputImages READ inputImages WRITE setInputImages NOTIFY inputImagesChanged FINAL)
 public:
     AbstractCVInputParams(QObject *parent = nullptr)
         : AbstractInputParams(parent)
@@ -23,18 +22,7 @@ public:
         return "AbstractCVInputParams";
     }
 
-    QVariant inputImages() const
-    {
-        return image_path_;
-    }
-
-    bool setInputImages(const QVariant &value);
-
 protected:
-    QVariant image_path_{""};
-
-signals:
-    void inputImagesChanged();
 };
 
 class QUICKTOOLS_CORE_EXPORT AbstractCVOutputParams : public AbstractOutputParams
@@ -44,7 +32,6 @@ class QUICKTOOLS_CORE_EXPORT AbstractCVOutputParams : public AbstractOutputParam
     QML_NAMED_ELEMENT(CVOutputParams)
     QML_UNCREATABLE("Can't not create a AbstractCVOutputParams directly")
 
-    Q_PROPERTY(QVariant outputImages READ outputImages NOTIFY outputImagesChanged FINAL)
 public:
     AbstractCVOutputParams(QObject *parent = nullptr)
         : AbstractOutputParams(parent)
@@ -58,18 +45,7 @@ public:
         return "AbstractCVOutputParams";
     }
 
-    bool setOutputImages(const QVariant &value);
-
-    QVariant outputImages() const
-    {
-        return output_data_;
-    }
-
 protected:
-    QVariant output_data_;
-
-signals:
-    void outputImagesChanged();
 };
 
 } // namespace quicktools::core
