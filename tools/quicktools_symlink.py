90c681e503fde3930512eeb2d41eb8cc27de3d67
wangtao <595517713@qq.com>
2024-05-08 15:57:50 +0800

    修改链接脚本，直接链接顶级目录

--- tools/quicktools_symlink.py
+++ tools/quicktools_symlink.py
@@ -1,15 +1,14 @@
 import argparse
 from pathlib import Path
 
-
-def dir_symlinks(link_dir, target_dir):
+def sub_dir_symlinks(link_dir, target_dir):
     link_dir = Path(link_dir)
     if not link_dir.exists():
         link_dir.mkdir(parents=True)
     target_dir = Path(target_dir)
-    symlink_dirs = [subdir.stem for subdir in target_dir.iterdir() if subdir.is_dir()]
+    subdirs_to_link = [subdir.stem for subdir in target_dir.iterdir() if subdir.is_dir()]
 
-    for stem in symlink_dirs:
+    for stem in subdirs_to_link:
         link = link_dir / stem
         target = target_dir / stem
         link = link.absolute()
@@ -22,6 +21,20 @@ def dir_symlinks(link_dir, target_dir):
         else:
             print(f'create symlink {link} to {target}')
         link.symlink_to(target, target_is_directory=True)
+
+
+def dir_symlinks(link_dir, target_dir):
+    link_dir = Path(link_dir)
+    target_dir = Path(target_dir)
+    link = link_dir.absolute()
+    target = target_dir.absolute()
+    if link.exists():
+        link.unlink()
+    if not target.exists():
+        print(f'failed to create symlink, not found {target}')
+    else:
+        print(f'create symlink {link} to {target}')
+    link.symlink_to(target, target_is_directory=True)
 
 if __name__ == '__main__':
     dir_symlinks('./build/bin/QuickTools', './build/QuickTools')
