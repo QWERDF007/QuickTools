f3affa035801caacbccb50626affbb316d48d5ac
wangtao <595517713@qq.com>
2024-05-30 17:58:00 +0800

    修复资源未释放的问题

--- src/common/Singleton.h
+++ src/common/Singleton.h
@@ -9,7 +9,19 @@ template<typename T>
 class Singleton
 {
 public:
+    /**
+     * @brief 返回单例指针
+     * @note: 不能使用返回静态局部变量的指针, 否则结束时会报错 _CrtlsValidHeapPointer(block),
+     *        应该是 Qt 对指针进行了 delete, 然后结束时静态变量又自己 delete, 导致 double delete.
+     * @return
+     */
     static T *getInstance();
+
+    /**
+     * @brief 返回单例指针, 需要使用静态局部变量的指针, 否则最终不会释放资源
+     * @return
+     */
+    static T *getInstanceCPP();
 };
 
 template<typename T>
@@ -19,18 +31,31 @@ T *Singleton<T>::getInstance()
     return instance;
 }
 
+template<typename T>
+T *Singleton<T>::getInstanceCPP()
+{
+    static T instance = T();
+    return &instance;
+}
+
 } // namespace quicktools::common
 
-#define SINGLETON(Class)                                            \
-private:                                                            \
-    friend class quicktools::common::Singleton<Class>;              \
-                                                                    \
-public:                                                             \
-    static Class *getInstance()                                     \
-    {                                                               \
-        return quicktools::common::Singleton<Class>::getInstance(); \
+/**
+ * @brief c++ 单例
+ */
+#define SINGLETON(Class)                                               \
+private:                                                               \
+    friend class quicktools::common::Singleton<Class>;                 \
+                                                                       \
+public:                                                                \
+    static Class *getInstance()                                        \
+    {                                                                  \
+        return quicktools::common::Singleton<Class>::getInstanceCPP(); \
     }
 
+/**
+ * @brief Qt QML 单例
+ */
 #define QT_QML_SINGLETON(Class)                                      \
 private:                                                             \
     friend class quicktools::common::Singleton<Class>;               \
