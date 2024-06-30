1ab6322aa0da21a131c8aa6ec99b3d71eacf7634
wangtao <595517713@qq.com>
2024-06-30 11:47:52 +0800

    添加判断是否找到python

--- cmake/ConfigPython.cmake
+++ cmake/ConfigPython.cmake
@@ -1,2 +1,6 @@
 # 指定 pybind11 的 python环境
 set(PYTHON_EXECUTABLE "H:/Software/Anaconda3/envs/ad/python.exe")
+file(EXISTS ${PYTHON_EXECUTABLE} PYTHON_EXISTS)
+if(NOT PYTHON_EXISTS)
+  message(FATAL_ERROR "Python executable not found: ${PYTHON_EXECUTABLE}")
+endif()
