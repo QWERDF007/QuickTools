569b1babef06607bb33af7cbba3a8381bb2a25db
wangtao <595517713@qq.com>
2024-06-30 12:39:17 +0800

    修改Qt版本，设置资源策略

--- cmake/ConfigQT.cmake
+++ cmake/ConfigQT.cmake
@@ -1,20 +1,26 @@
 # 设置 Qt 的 cmake 文件目录
-set(QT_CMAKE_DIR "H:/Software/Qt/6.6.1/msvc2019_64/lib/cmake/Qt6")
-set(QT_QML_IMPORT_DIR "H:/Software/Qt/6.6.1/msvc2019_64/qml")
+set(Qt6_CMAKE_DIR  "E:/Softwares/Qt/6.7.2/msvc2019_64/lib/cmake")
+set(Qt6_DIR  "${Qt6_CMAKE_DIR}/Qt6")
+set(Qt6QmlTools_DIR  "${Qt6_CMAKE_DIR}/Qt6QmlTools")
+set(QT_QML_IMPORT_DIR "E:/Softwares/Qt/6.7.2/msvc2019_64/qml")
 
 
 # 寻找Qt6的Quick组件
-find_package(Qt6 6.5 REQUIRED COMPONENTS Core Gui Quick Widgets Charts PATHS ${QT_CMAKE_DIR})
+find_package(Qt6  REQUIRED COMPONENTS Core Gui Quick Widgets Charts)
 
 # 寻找Qt6的测试组件
-find_package(Qt6 6.5 REQUIRED COMPONENTS QuickTest)
+find_package(Qt6  REQUIRED COMPONENTS QuickTest)
 
 # 设置Qt6项目
-qt_standard_project_setup(REQUIRES 6.5)
+qt_standard_project_setup(REQUIRES 6)
 
 set(CMAKE_AUTOMOC ON)
 set(CMAKE_AUTORCC ON)
 set(CMAKE_AUTOUIC ON)
+
+
+# 设置 QTP0001 策略为 NEW，即使使用新的资源前缀 :/qt/qml/ 作为默认值
+qt_policy(SET QTP0001 NEW)
 
 # 添加 qml 导入目录
 # QML_IMPORT_PATH 用于支持语法高亮
