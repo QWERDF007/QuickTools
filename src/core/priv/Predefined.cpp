ca477453ed07f8d41a3ef9bbeec089b41c723b27
wangtao <595517713@qq.com>
2024-07-18 14:28:37 +0800

    使用类静态变量消除警告

---
+++ src/core/priv/Predefined.cpp
@@ -0,0 +1,6 @@
+#include "Predefined.h"
+
+namespace quicktools::core {
+const QString Predefined::RUN_AFTER_CHANGED = "RunAfterChanged";
+const QString Predefined::PYTHON_HOME       = "PYTHON_HOME";
+} // namespace quicktools::core
--- src/core/priv/Predefined.h
+++ src/core/priv/Predefined.h
@@ -1,14 +1,13 @@
 #pragma once
+
 #include <QString>
 
-#define DEBUG_MSG
-
-#ifdef DEBUG_MSG
-#    define Critical(MSG) qCritical() << __FUNCTION__ << __LINE__ << "msg:" << MSG
-#else
-#    define Critical(MSG)
-#endif
-
 namespace quicktools::core {
-const QString RUN_AFTER_CHANGED = "RunAfterChanged";
+class Predefined
+{
+public:
+    static const QString RUN_AFTER_CHANGED;
+    static const QString PYTHON_HOME;
+};
+
 } // namespace quicktools::core
