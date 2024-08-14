f2585dbda3584a080367a6a168f42c2be686965b
wangtao <595517713@qq.com>
2024-08-14 22:26:53 +0800

    修改ROI绘制的默认颜色和alpha

--- src/core/CVToolSettings.cpp
+++ src/core/CVToolSettings.cpp
@@ -18,10 +18,10 @@ CVToolSettings::~CVToolSettings() {}
 void CVToolSettings::addCVBasicSettings()
 {
     addGroup(SettingsGroup::DrawingGroup, tr("绘制设置"));
-    addColorDialogSetting(SettingsGroup::DrawingGroup, "ROIColor", tr("ROI 颜色"), "", QColor("red"));
-    addColorDialogSetting(SettingsGroup::DrawingGroup, "ROIBorderColor", tr("ROI 边框颜色"), "", QColor("red"));
+    addColorDialogSetting(SettingsGroup::DrawingGroup, "ROIColor", tr("ROI 颜色"), "", QColor("#ff08ff"));
+    addColorDialogSetting(SettingsGroup::DrawingGroup, "ROIBorderColor", tr("ROI 边框颜色"), "", QColor("#ff08ff"));
     addSliderSetting(SettingsGroup::DrawingGroup, "ROIColorAlpha", tr("ROI 颜色 Alpha"),
-                     tr("ROI 颜色的 Alpha 数值, 修改 ROI 的不透明度"), 0.5, 0.0, 1.0, 0.1);
+                     tr("ROI 颜色的 Alpha 数值, 修改 ROI 的不透明度"), 0.1, 0.0, 1.0, 0.05);
 }
 
 } // namespace quicktools::core
