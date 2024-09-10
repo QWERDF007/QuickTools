90d9b3adc8009d284c5fc9358a4101c0abfa8a8c
wangtao <595517713@qq.com>
2024-09-10 11:03:59 +0800

    修改调用类

--- src/py_module/yolov8_detection.py
+++ src/py_module/yolov8_detection.py
@@ -1,4 +1,4 @@
-from ultralytics import YOLO
+import torch
 from ultralytics.models.yolo.detect import DetectionPredictor
 
 class Yolov8DetectionPredictor:
@@ -6,21 +6,25 @@ class Yolov8DetectionPredictor:
         self.init_model(model_path, imgsz, device)
     
     def init_model(self, model_path, imgsz, device):
+        print(f"Loading YOLOv8 model from {model_path} on device {device}...", flush=True)
         self.model_path = model_path
         self.imgsz = imgsz
         self.device = device
-        self.model = YOLO(self.model_path)
-        
+        cfg = {'batch': 1, 'model': self.model_path, 'conf': 0.25, 'iou': 0.7, 'imgsz': self.imgsz, 
+                'device': self.device,'save_txt': False, 'visualize': False, 'save': False, 'verbose': False}
+        self.model = DetectionPredictor(overrides=cfg)
 
+    @torch.no_grad()
     def predict(self, img, conf, iou):
-        print(img.shape, flush=True)
-        result = self.model(img, conf=conf, iou=iou, device=self.device)
+        self.model.args.conf = conf
+        self.model.args.iou = iou
+        result = self.model(img)
         boxes = result[0].boxes
         ret = {
             'status': 0,
             'msg': 'ok',
             'boxes': boxes.xyxy.tolist(),
-            'cls': boxes.cls.tolist(),
+            'cls': boxes.cls.int().tolist(),
             'conf': boxes.conf.tolist()
         }
         print(ret, flush=True)
