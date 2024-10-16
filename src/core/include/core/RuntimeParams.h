8367266f8076dcb02e3e6d93490196b7441737ca
wangtao <595517713@qq.com>
2024-10-17 00:42:44 +0800

    修改returnError通过错误类生成错误信息

--- src/core/include/core/RuntimeParams.h
+++ src/core/include/core/RuntimeParams.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include <QString>
+#include "Error.h"
+
 #include <QObject>
+#include <QString>
 
 namespace quicktools::core {
 
@@ -14,35 +16,28 @@ public:
     RuntimeParams(QObject *parent = nullptr)
         : QObject(parent)
     {
-
     }
 
-    virtual ~RuntimeParams()
-    {
+    virtual ~RuntimeParams() {}
 
-    }
+    virtual void reset() {}
 
     virtual bool getInput(InputParams *input_params)    = 0;
     virtual bool genOutput(OutputParams *output_params) = 0;
 
-    virtual void reset()
-    {
-
-    }
-
     void setError(const int error, const QString &msg = QString())
     {
         error_ = error;
-        msg_ = msg;
+        msg_   = msg;
     }
 
     std::tuple<int, QString> returnError() const
     {
-        return {error_, msg_};
+        return {error_, Error::ErrorGenerator::getErrorString(error_, msg_)};
     }
 
 private:
-    int error_{0};
+    int     error_{0};
     QString msg_;
 };
 
